99421a11e4a19c3feec730c5830723ff38a2d3ad
lyxuncle <lyxuncle@gmail.com>
2015-12-25 21:06:53 +0800

    封面数据

--- mobi/js/vol_maga.js
+++ mobi/js/vol_maga.js
@@ -1,3 +1,3 @@
 [
-	{"vol": 4, "date": "2015-12-21", "prewords": "新年新气象～（是不是有点早", "cover": "img/example.png", "hexocolor": ["FE4C4A", "AF004D"]}
+	{"vol": 4, "date": "2015-12-21", "prewords": "新年新气象～（是不是有点早", "cover": "images/cover/4.png", "hexocolor": ["FE4C4A", "AF004D"]}
 ]
